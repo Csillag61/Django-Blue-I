217fb530520dffcc7cae7ca41653e141494aebb7
Csillag61 <csillagok@gmail.com>
2018-12-03 11:25:31 -0500

    models migrations

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264,unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class Webpage(models.Model):
+        # topic = models.ForeignKey(Topic, on_delete=models.CASCADE,)
+        name = models.CharField(max_length=264, unique=True)
+        url = models.URLField(unique=True)
+
+ #for string representation foreign key needs on_delete=models.CASCADE,
+
+        def __str__(self):
+            return str(self.name)
+
+class AccessRecord(models.Model):
+        # name = models.ForeignKey(models.Model, on_delete=models.CASCADE,)
+        date = models.DateField()
+
+        def __str__(self):
+            return str(self.date)
+
